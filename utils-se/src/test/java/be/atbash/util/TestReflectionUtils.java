020a8c29c41e57cfd177bd494889f35cc50f63bf
Rudy De Busscher <rdebusscher@gmail.com>
2018-02-22 21:37:08 +0100

    TestReflectionUtils.injectDependencies

--- utils-se/src/test/java/be/atbash/util/TestReflectionUtils.java
+++ utils-se/src/test/java/be/atbash/util/TestReflectionUtils.java
@@ -25,6 +25,32 @@ public final class TestReflectionUtils {
     private TestReflectionUtils() {
     }
 
+    /**
+     * Injects objects into the target (private) fields by matching the type.
+     *
+     * @param target       The target object where the dependencies are injected
+     * @param dependencies The objects we like to set into the target.
+     * @throws IllegalAccessException Should not happen since we overrule the accessibility
+     */
+    public static void injectDependencies(final Object target, final Object... dependencies) throws IllegalAccessException {
+        Class targetClass = target.getClass();
+        while (targetClass != null && targetClass != Object.class) {
+            if (targetClass.getName().contains("$")) {
+                targetClass = targetClass.getSuperclass();
+            }
+            for (Field field : targetClass.getDeclaredFields()) {
+                field.setAccessible(true);
+                for (Object dependency : dependencies) {
+                    if (field.getType().isAssignableFrom(dependency.getClass())) {
+                        field.set(target, dependency);
+                    }
+                }
+
+            }
+            targetClass = targetClass.getSuperclass();
+        }
+    }
+
     public static Object getValueOf(Object instance, String fieldName) throws NoSuchFieldException, IllegalAccessException {
         Object target;
         Class<?> targetClass;
