34aa82d7a7c058991fc36c3dc0ebb5d52be32d47
Rudy De Busscher <rdebusscher@gmail.com>
2020-11-07 19:08:28 +0100

    Allow to set beanManager mock in class BeanManagerFake (Enrico Comiti). Fixes #16

--- utils-cdi/src/test/java/be/atbash/util/BeanManagerFake.java
+++ utils-cdi/src/test/java/be/atbash/util/BeanManagerFake.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2019 Rudy De Busscher (https://www.atbash.be)
+ * Copyright 2014-2020 Rudy De Busscher (https://www.atbash.be)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,7 +55,11 @@ public class BeanManagerFake {
     private Map<String, Object> registeredBeans;
 
     public BeanManagerFake() {
-        beanManagerMock = mock(BeanManager.class);
+        this(mock(BeanManager.class));
+    }
+
+    public BeanManagerFake(BeanManager beanManager) {
+        beanManagerMock = beanManager;
 
         registeredObjects = new HashMap<>();
         registeredBeans = new HashMap<>();
